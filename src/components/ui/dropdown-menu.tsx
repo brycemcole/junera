a733af441673fd71e52fdb5063531c3cca91ffe3
brycemcole <brycemcole@icloud.com>
2025-02-08 14:37:09 -0500

    fix for dropdown background

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -221,7 +221,7 @@ const DropdownMenuShortcut = ({ className, ...props }: React.HTMLAttributes<HTML
   return (
     <span
       className={cn(
-        "-me-1 ms-auto inline-flex h-5 max-h-full items-center rounded border border-border bg-background px-1 font-[inherit] text-[0.625rem] font-medium text-muted-foreground/70",
+        "-me-1 ms-auto inline-flex h-5 max-h-full items-center rounded border border-border px-1 font-[inherit] text-[0.625rem] font-medium text-muted-foreground/70",
         className,
       )}
       {...props}
